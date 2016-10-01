283f0159796f66763c2cf6115b00793825b3e22c
Harsh Vardhan <harsh59v@gmail.com>
2016-10-01 20:01:00 +0530

    Successfully writes offline sayings to the TextView.

--- app/src/main/java/com/vharsh/quoterialist/MainActivity.java
+++ app/src/main/java/com/vharsh/quoterialist/MainActivity.java
@@ -1,78 +1,90 @@
 package com.vharsh.quoterialist;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
 import android.widget.TextView;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-
 public class MainActivity extends AppCompatActivity {
-	private static final String url = "http://api.forismatic.com/api/1.0/?method=getQuote&jsonp=parseQuote&format=jsonp&lang=en";
-
+    TextView Inspiration, Model;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView Inspiration = (TextView) findViewById(R.id.lines);
-        TextView Model = (TextView) findViewById(R.id.speaker);
-        String quote[] = new String[2];
-        quote = GetQuote();
-        // Populate quote with one of the 12 quotes ! Yay
-        if (quote == null) {
-            quote = OfflineQuote();
-        }
+        Inspiration = (TextView) findViewById(R.id.lines);
+        Model = (TextView) findViewById(R.id.speaker);
+        final String url = getResources().getString(com.vharsh.quoterialist.R.string.quote_url);
 
-    }
-        /**
-         * Requests an english Quote from forismatic API 1.0 as a string array using HTTPURLConnection
-    	 * <i> This is called only when the device is Online </i>
-         * @return {"quote", "author"}, or <b>null</b> if HTTP connection fails
-         */
-        public static String[] GetQuote(){
-                try {
-                    URL obj = new URL(url);
-                    int dum;
-                    String inputLine;
-                    StringBuffer response = new StringBuffer();
-                    HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-                    con.setRequestMethod("GET");
-                    con.setRequestProperty("User-Agent", "Mozilla/5.0");
-                    int responseCode = con.getResponseCode();
-                    //System.out.println("Response Code : " + responseCode);
-                    if (responseCode != 200)
-                        return null;
-                    BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-                    while ((inputLine = in.readLine()) != null) {
-                        response.append(inputLine);
-                    }
-                    System.err.println("HERE IT GOES");
-                    //System.err.println(new String(response));
-                    response = (response.replace(0, 11, ""));
-                    response.setLength(Math.max(response.length() - 1, 0));
-                    String reply[] = new String[2];
-                    reply[0] = response.substring(14, response.indexOf("\",", 14));
-                    dum = response.indexOf(", \"quoteAuthor\":\"") + 17;
-                    reply[1] = response.substring(dum, response.indexOf("\",", dum));
-                    in.close();
-                    return reply;
-                }
-                catch (Exception e){
-                    // Can you even do something?
-                    return null;
-                }
-	}
-    private static String[] OfflineQuote(){
-        int i = int(Math.random() * 12);
-        File f = new File("offlineQuotes");
-        FileInputStream fis = new FileInputStream(f);
-        BufferedReader brr = new BufferedReader(fis);
-        for (int j = 0; j < i; j++){
-
+        if (findNet()) {
+            // Call the function onlineQuote()
+        } else {
+            offlineQuote();
         }
     }
+
+    /**
+     * Checks for internet connectivity each time the app is loaded.
+     *
+     * @return false if the mobile is disconnected
+     */
+    private boolean findNet() {
+        ConnectivityManager cm =
+                (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        return activeNetwork != null && activeNetwork.isConnectedOrConnecting();
+    }
+
+    /**
+     * A single interface which writes sayings to the TextViews
+     */
+    private void writeOut(String message, String author) {
+        Inspiration.setMovementMethod(new ScrollingMovementMethod());
+        Inspiration.setText(message);
+        Model.setText(author);
+    }
+
+    /**
+     * Changes the value of the quote[] to a (hopefully)new quote and it's speaker, i.e. 'quote-mouth'.
+     * <i>Picks up a random pop saying, from an array.</i>
+     */
+    void offlineQuote() {
+        int i = (int) (Math.random() * 10);
+        String quotNot[] = {"The Internet is so big, so powerful and pointless that for some people it is a complete substitute for life.",
+                "Andrew Brown",
+                "We are all now connected by the Internet, like neurons in a giant brain.",
+                "Stephen Hawking",
+                "Big stories need human stakes.",
+                "Aaron Swartz",
+                "Information is power. But like all power, there are those who want to keep it for themselves.",
+                "Aaron Swartz",
+                "It's far better to buy a wonderful company at a fair price than a fair company at a wonderful price.",
+                "Warren Buffett",
+                "The first rule is not to lose. The second rule is not to forget the first rule.",
+                "Warren Buffett",
+                "Price is what you pay. Value is what you get.",
+                "Warren Buffett",
+                "There are three constants in life... change, choice and principles.",
+                "Stephen Covey",
+                "You can retire from a job, but don't ever retire from making extremely meaningful contributions in life.",
+                "Stephen Covey",
+                "Chance favors the connected mind.",
+                "Steven Johnson",
+                "Google can bring you back 100,000 answers. A librarian can bring you back the right one.",
+                "Neil Gaiman"
+        };
+        writeOut(quotNot[(2 * i)], quotNot[(2 * i + 1)]);
+    }
+
+    public void OnClick(View view) {
+        if (findNet()) {
+            // Call the function onlineQuote()
+        } else {
+            offlineQuote();
+        }
+    }
+
 }
